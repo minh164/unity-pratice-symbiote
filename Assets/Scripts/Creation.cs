2b3b46f80eb902c18e2b6bf7fd334dccbb609785
Minh Mai <minh.mai@saritasa.com>
2024-06-10 00:02:19 +0700

    check existed joint in bone

--- Assets/Scripts/Creation.cs
+++ Assets/Scripts/Creation.cs
@@ -420,12 +420,29 @@ public class Creation : MonoBehaviour
 
     private void CreateJoint(GameObject bone, GameObject connectedBone)
     {
+        // If Current Bone has connected with Connected Bone OR reverse, it will not do again.
+        if (IsExistedJoint(bone, connectedBone) || IsExistedJoint(connectedBone, bone)) {
+            return;
+        }
+        
         bone.AddComponent<SpringJoint>();
         SpringJoint[] joints = bone.GetComponents<SpringJoint>();
-        SpringJoint joint = joints[joints.Length - 1];
-        joint.connectedBody = connectedBone.GetComponent<Rigidbody>();
-        joint.autoConfigureConnectedAnchor = autoAnchor;
-        joint.spring = spring;
+        SpringJoint newJoint = joints[joints.Length - 1];
+        newJoint.connectedBody = connectedBone.GetComponent<Rigidbody>();
+        newJoint.autoConfigureConnectedAnchor = autoAnchor;
+        newJoint.spring = spring;
+    }
+
+    private bool IsExistedJoint(GameObject bone, GameObject connectedBone)
+    {
+        SpringJoint[] joints = bone.GetComponents<SpringJoint>();
+        foreach (SpringJoint joint in joints) {
+            if (joint.connectedBody.GetInstanceID() == connectedBone.GetComponent<Rigidbody>().GetInstanceID()) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     private Vector3[] SpherizeVectors(Vector3[] vectors)
